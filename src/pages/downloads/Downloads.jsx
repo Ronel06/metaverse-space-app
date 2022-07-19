338fcd77316aefb5a684370f231fe095a0403f6f
EnzGyb <rdimasapit.bos@gmail.com>
2022-07-19 15:48:56 +0800

    initial commit

---
+++ src/pages/downloads/Downloads.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Downloads.css'
+
+const Downloads = () => {
+  return (
+    <div>Downloads</div>
+  )
+}
+
+export default Downloads
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import Home from './home/Home';
+import About from './about/About';
+import Account from './account/Account';
+import Contact from './contact/Contact';
+import Downloads from './downloads/Downloads';
+import Register from './register/Register';
+import Login from './login/Login';
+
+export { Home, About, Account, Contact, Downloads, Register, Login }
