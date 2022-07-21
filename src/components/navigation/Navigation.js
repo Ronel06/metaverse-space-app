61dbb7c077949f7f8ff72cab36510d2cbcf51509
Ronel Bargamento <107118194+Ronel06@users.noreply.github.com>
2022-07-21 19:22:33 +0800

    home footer header
    home footer header

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,9 +1,39 @@
 import React from 'react'
 import './Navigation.css'
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
+import logo from '../../assets/images/landing/header/logo.png';
+
+
+
 
 const Navigation = () => {
   return (
-    <div>Navigation</div>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <div className="container-fluid">
+        <Navbar.Brand className="logo-header" href="#home"><img src={logo}></img></Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link className="nav-font  me-2" href="#features">ABOUT</Nav.Link>
+            <Nav.Link className="nav-font  me-2" href="#download">DOWNLOAD</Nav.Link>
+            <Nav.Link className="nav-font  me-2" href="#news&events">NEWS&EVENTS</Nav.Link>
+            <Nav.Link className="nav-font  me-2" href="#contactus">CONTACT US</Nav.Link>
+          </Nav>
+          <Nav>
+            <div>
+              <Button className="btn-login me-2" variant="primary" size="sm">
+                Login
+              </Button>
+              <Button className="btn-signup" variant="secondary" size="sm">
+                Register
+              </Button>
+            </div>
+          </Nav>
+        </Navbar.Collapse>
+        </div>
+    </Navbar>
   )
 }
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import image1 from '../../assets/images/landing/content/image1.png';
 import "./Home.css";
 
 const Home = () => {
   return (
-    <div className="container-fluid custom-section-size custom-header-bg">
-      <div className="row vh-100 d-flex align-items-center">
+
+    <div className="container-fluid">
+      <div className="custom-section-size">
+      <div className="row vh-100 d-flex align-items-center  custom-header-bg">
         <div className="col-md-6 ms-0 ms-md-5">
           <h1 className="custom-header-title">A Virtual Metaverse</h1>
           <p className="custom-header-paragraph">
@@ -16,6 +19,24 @@ const Home = () => {
         </div>
       </div>
     </div>
+
+    <div className="custom-section-size">
+      <div className="row vh-100 d-flex align-items-center custom-header-bg2">
+       <div className="section-cards d-flex flex-row justify-content-center">
+          <div className="card text-center w-25">
+              <div><img class="card-img-top" src={image1}></img></div> 
+               <div className="card-body align-items-center">
+               <h5 class="card-title">Card title</h5>
+                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+               </div>
+          </div>
+       </div>
+      </div>
+    </div>
+    
+
+
+  </div>
   );
 };
 
